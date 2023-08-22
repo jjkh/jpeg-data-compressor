53f63668758da758a372b0ff5b388440bd6493de
Jamie Harvey <jharvey@duality.co.nz>
2023-08-22 22:49:31 +1200

    Get "compression" working

--- src/main.zig
+++ src/main.zig
@@ -2,32 +2,93 @@ const std = @import("std");
 const c = @cImport({
     @cDefine("STB_IMAGE_IMPLEMENTATION", "");
     @cInclude("stb_image.h");
-    @cInclude("jo_jpeg.h");
+    @cDefine("STB_IMAGE_WRITE_IMPLEMENTATION", "");
+    @cInclude("stb_image_write.h");
 });
 
-pub fn main() !void {
+pub fn main() !u8 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
     const args = parseArgs(allocator) catch |err| {
         try displayHelp(if (err == error.NoArguments) null else err);
-        return err;
+        return 1;
     };
-    std.debug.print("op: {}, infile: {s}, outfile: {s}", .{ args.operation, args.infile, args.outfile });
-    // if (args.operation == .extract) {
-    //     const ppm_image = try extractFileAsPpm(args.infile),
+    std.log.debug("op: {}, infile: {s}, outfile: {s}", .{ args.operation, args.infile(), args.outfile() });
+    if (args.operation == .compress) {
+        try compressFile(allocator, args);
+    } else {
+        try extractFile(allocator, args);
+    }
 
-    // }
+    return 0;
+}
+
+fn writeJpegData(context: ?*anyopaque, data: ?*anyopaque, size: c_int) callconv(.C) void {
+    const file: *std.fs.File = @alignCast(@ptrCast(context.?));
+    const data_to_write: [*]u8 = @ptrCast(data.?);
+    file.writeAll(data_to_write[0..@intCast(size)]) catch |err| std.debug.panic("failed to write data: {}", .{err});
+}
+
+fn compressFile(allocator: std.mem.Allocator, args: Args) !void {
+    std.log.info("opening file {s}", .{args.infile()});
+    const infile = try std.fs.cwd().openFile(args.infile(), .{});
+    defer infile.close();
+
+    const file_stat = try infile.stat();
+    const file_size = file_stat.size;
+
+    const img_size: u32 = @intFromFloat(@ceil(@sqrt(@as(f32, @floatFromInt(file_size)))));
+    std.log.debug(
+        "file size: {0}B, image size: {1}x{1}, {2} bytes wasted",
+        .{ file_size, img_size, img_size * img_size - file_size },
+    );
+
+    var data = try allocator.alloc(u8, img_size * img_size);
+    defer allocator.free(data);
+
+    @memset(data, '=');
+    _ = try infile.readAll(data);
+
+    std.log.info("creating file {s}", .{args.outfile()});
+    var outfile = try std.fs.cwd().createFile(args.outfile(), .{});
+    defer outfile.close();
+
+    const result = c.stbi_write_jpg_to_func(
+        writeJpegData,
+        &outfile,
+        @intCast(img_size),
+        @intCast(img_size),
+        1,
+        data.ptr,
+        90,
+    );
+    if (result == 0) return error.WriteFailed;
+}
+
+fn extractFile(allocator: std.mem.Allocator, args: Args) !void {
+    _ = args;
+    _ = allocator;
+    // const infile = try std.fs.cwd().openFile(args.infile, .{});
+    // defer infile.close();
 }
 
 const Args = struct {
     operation: enum { extract, compress },
-    infile: []const u8,
-    outfile: []const u8,
 
     infile_buf: [std.fs.MAX_PATH_BYTES]u8,
+    infile_len: usize,
     outfile_buf: [std.fs.MAX_PATH_BYTES]u8,
+    outfile_len: usize,
+
+    pub inline fn infile(self: Args) []const u8 {
+        return self.infile_buf[0..self.infile_len];
+    }
+
+    pub inline fn outfile(self: Args) []const u8 {
+        return self.outfile_buf[0..self.outfile_len];
+    }
 };
 
 fn parseArgs(allocator: std.mem.Allocator) !Args {
@@ -48,16 +109,16 @@ fn parseArgs(allocator: std.mem.Allocator) !Args {
 
     const infile = args_it.next() orelse return error.MissingArgument;
     @memcpy(args.infile_buf[0..infile.len], infile);
-    args.infile = args.infile_buf[0..infile.len];
+    args.infile_len = infile.len;
 
     if (args_it.next()) |outfile| {
         @memcpy(args.outfile_buf[0..outfile.len], outfile);
-        args.outfile = args.outfile_buf[0..outfile.len];
+        args.outfile_len = outfile.len;
     } else {
         const ext = if (args.operation == .compress) ".jpg" else ".out";
-        @memcpy(args.outfile_buf[0..args.infile.len], args.infile);
-        @memcpy(args.outfile_buf[args.infile.len..][0..ext.len], ext);
-        args.outfile = args.outfile_buf[0 .. args.infile.len + ext.len];
+        @memcpy(args.outfile_buf[0..args.infile_len], args.infile());
+        @memcpy(args.outfile_buf[args.infile_len..][0..ext.len], ext);
+        args.outfile_len = args.infile_len + ext.len;
     }
 
     if (args_it.next() != null)
